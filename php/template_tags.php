1b1ca7d02d60950416b23cfa9b4c44440be3880f
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-10 12:20:39 -0700

    Add support for Co-Authors Plus and relative timestamps

--- php/template_tags.php
+++ php/template_tags.php
@@ -15,3 +15,29 @@ function ona11_head_title() {
 	echo '<title>' . $title . '</title>';
 	
 }
+
+function ona11_author_posts_link() {
+	
+	if ( function_exists( 'coauthors_posts_links' ) )
+		coauthors_posts_links();
+	else
+		the_author_posts_link();
+}
+
+function ona11_timestamp( $type = 'long', $show_updated = true ) {
+	global $post;
+	$post_id = $post->ID;
+
+	$post_timestamp = get_post_time( 'U', true, $post_id );
+	$current_timestamp = time();
+
+	// Only do the relative timestamps for 7 days or less, then just the month and day
+	if ( $post_timestamp > ( $current_timestamp - 604800 ) ) {
+		echo human_time_diff( $post_timestamp ) . ' ago';
+	} else if ( $post_timestamp > ( $current_timestamp - 220752000 ) ) {
+		the_time( 'F jS' );
+	} else {
+		the_time( 'F j, Y' );
+	}
+	
+}
