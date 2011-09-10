aa00d4c22f83a63b6e5eaa454934cd4b2374f381
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-10 11:25:43 -0700

    Deprecate use of sandbox_post_class and sandbox_body_class in favor of post_class and body_clas

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 
 <?php the_post() ?>
 
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+			<div id="post-<?php the_ID() ?>" <?php post_class(); ?>>
 				<h2 class="entry-title"><?php the_title() ?></h2>
 				<div class="entry-content">
 <?php the_content() ?>
--- tag.php
+++ tag.php
@@ -12,7 +12,7 @@
 
 <?php while ( have_posts() ) : the_post() ?>
 
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+			<div id="post-<?php the_ID() ?>" <?php post_class(); ?>>
 				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __( 'Permalink to %s', 'sandbox' ), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h3>
 				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
 				<div class="entry-content">
