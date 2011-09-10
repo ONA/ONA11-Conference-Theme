7a87e93bce6ac5098bb4ccf66c836c283526854e
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-10 14:35:04 -0700

    Show the last modified date if there's a difference and make it clickable

--- php/template_tags.php
+++ php/template_tags.php
@@ -43,6 +43,24 @@ function ona11_timestamp( $type = 'long', $show_updated = true ) {
 		$html .= get_the_time( 'F j, Y' );
 	}
 	$html .= '</span><span class="full-date">' . get_the_time( 'l, F j, Y' ) . ' at ' . get_the_time( 'g:i a T' ) . '</span></span>';
+	if ( !$show_updated || ( get_the_time( 'U' ) == get_the_modified_time( 'U' ) ) ) {
+		echo $html;
+		return;
+	}
+	
+	$post_modified_timestamp = get_post_modified_time( 'U', true, $post_id );
+	$html .= ', <span class="post-modified-date clickable-date">last modified <span class="readable-date">';
+	if ( get_post_modified_time( 'l, F j, Y', true, $post_id ) == date( 'l, F j, Y' ) ) {
+		$html .= human_time_diff( $post_modified_timestamp ) . ' ago';
+	} else if ( $post_modified_timestamp > ( $current_timestamp - 518400 ) ) {
+		$html .= get_the_modified_time( 'l' ) . ' at ' . get_the_modified_time( 'g:i a T' );
+	} else if ( $post_modified_timestamp > ( $current_timestamp - 220752000 ) ) {
+		$html .= get_the_modified_time( 'F jS' );
+	} else {
+		$html .= get_the_modified_time( 'F j, Y' );
+	}
+	$html .= '</span><span class="full-date">' . get_the_modified_time( 'l, F j, Y' ) . ' at ' . get_the_modified_time( 'g:i a T' ) . '</span></span>';
+	
 	echo $html;
 	
 }
