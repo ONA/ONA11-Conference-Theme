1cf699c97bee2ff077a7314940cf2fcf437b3222
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-14 21:14:29 -0700

    Better session navigation at the top

--- loop-single_session.php
+++ loop-single_session.php
@@ -1,24 +1,18 @@
 <?php if ( have_posts() ): ?>
 
 <div class="session-navigation">
-	<div class="session-navigation-previous session-navigation-link">
-	<?php if ( $previous_session_link = ona11_get_previous_session_link() ): ?>
- 		<a href="<?php echo $previous_session_link; ?>">&larr;</a>
-	<?php else: ?>
-		&larr;
-	<?php endif; ?>
-	</div>
-	<div class="session-navigation-home session-navigation-link">
-	<a href="<?php echo get_site_url( null, '/sessions/' ); ?>"><?php _e( 'All' ); ?><a>
-	</div>
-	<div class="session-navigation-previous session-navigation-link">	
-	<?php if ( $next_session_link = ona11_get_next_session_link() ): ?>
- 		<a href="<?php echo $next_session_link; ?>">&rarr;</a>
-	<?php else: ?>
-		&rarr;
-	<?php endif; ?>
-	</div>
-
+	<a href="<?php echo get_site_url( null, '/sessions/' ); ?>"><?php _e( 'All' ); ?></a>
+	<?php
+	$session_types = wp_get_post_terms( get_the_id(), 'ona11_session_types' );
+	if ( count( $session_types ) ) {
+		$session_types_html = ' &rarr; <a href="' . get_term_link( $session_types[0] ) . '">' . esc_html( $session_types[0]->name ) . '</a>';
+		if ( $session_types[0]->parent ) {
+			$parent_session = get_term_by( 'id', $session_types[0]->parent, 'ona11_session_types' );
+			$session_types_html = ' &rarr; <a href="' . get_term_link( $parent_session ) . '">' . esc_html( $parent_session->name ) . '</a>' . $session_types_html;
+		}
+		echo $session_types_html;
+	} 
+	?>
 </div>
 
 <?php while ( have_posts() ) : the_post(); ?>
@@ -43,19 +37,11 @@
 		} else {
 			$session_where = 'Location to be announced soon';
 		}
-		
-		$session_type_tax = wp_get_object_terms( get_the_id(), 'ona11_session_types' );
-		if ( count( $session_type_tax ) ) {
-			$session_which = '<a href="' . get_term_link( $session_type_tax[0] ) . '">' . esc_html( $session_type_tax[0]->name ) . '</a>';
-		} else {
-			$session_which = 'Not listed';
-		}
 	?>
 	
 	<h2 class="session-title"><?php the_title(); ?></h2>
 	<div class="session-meta session-when"><span class="label highlight">When:</span> <?php echo $session_when; ?></div>
 	<div class="session-meta session-where"><span class="label highlight">Where:</span> <?php echo $session_where; ?></div>
-	<div class="session-meta session-type"><span class="label highlight">Type:</span> <?php echo $session_which; ?></div>	
 
 	<div class="session-content">
 		<?php the_content(); ?>
