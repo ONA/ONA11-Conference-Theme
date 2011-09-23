a6c1a2568d48c2eddd32ed4e1887bf3024ef5b0e
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-23 13:12:03 -0400

    New header image

--- header.php
+++ header.php
@@ -31,10 +31,6 @@
 
 	<div class="header">		
 		<div class="wrap">
-			<?php if ( is_home() ): ?>
-			<a href="<?php bloginfo('url') ?>/"><img height="331px" src="<?php bloginfo('template_directory'); ?>/images/header_h927.jpg" /></a>
-			<?php else: ?>
 			<a href="<?php bloginfo('url') ?>/"><img height="150px" src="<?php bloginfo('template_directory'); ?>/images/header_small_h930.png" /></a>	
-			<?php endif; ?>
 		</div>
 	</div><!--	#header -->
