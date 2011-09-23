b76044ad160a4513ee6fb9dfe1d62c0bdbeb0908
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-23 08:28:05 -0400

    Implement post formats and partial implementation of galleries

--- functions.php
+++ functions.php
@@ -22,6 +22,16 @@ class ona11
 		$this->person = new ona11_person();
 		
 		add_theme_support( 'post-thumbnails' );
+		$formats = array(
+			'gallery',
+			'aside',
+			'quote',
+			'status',
+			'image',
+			'video',
+			'audio',
+		);
+		add_theme_support( 'post-formats', $formats );
 		add_image_size( 'home-featured', 400, 300, true );
 		add_image_size( 'small-square', 64, 64, true );				
 				
@@ -32,7 +42,9 @@ class ona11
 		add_action( 'widgets_init', array( &$this, 'action_widgets_init' ) );
 		add_action( 'admin_bar_menu', array( &$this, 'action_admin_bar_menu' ), 200 );	
 		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_resources' ) );
-		add_action( 'zoninator_pre_init', array( &$this, 'zoninator_pre_init' ) );	
+		add_action( 'zoninator_pre_init', array( &$this, 'zoninator_pre_init' ) );
+		
+		//add_filter( 'post_gallery', array( &$this, 'post_gallery' ) );	
 		
 		if ( !is_admin() ) {
 			require_once( 'php/template_tags.php' );
@@ -212,6 +224,115 @@ class ona11
 	function wp_head() {
 		echo '<script type="text/javascript" src="http://use.typekit.com/fey8mly.js"></script>';
 		echo '<script type="text/javascript">try{Typekit.load();}catch(e){}</script>';
+	}
+	
+	function post_gallery( $attr ) {
+		global $post;
+		// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+		if ( isset( $attr['orderby'] ) ) {
+			$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+			if ( !$attr['orderby'] )
+				unset( $attr['orderby'] );
+		}
+
+		extract(shortcode_atts(array(
+			'order'      => 'ASC',
+			'orderby'    => 'menu_order ID',
+			'id'         => $post->ID,
+			'itemtag'    => 'dl',
+			'icontag'    => 'dt',
+			'captiontag' => 'dd',
+			'columns'    => 3,
+			'size'       => 'thumbnail',
+			'include'    => '',
+			'exclude'    => ''
+		), $attr));
+
+		$id = intval($id);
+		if ( 'RAND' == $order )
+			$orderby = 'none';
+
+		if ( !empty($include) ) {
+			$include = preg_replace( '/[^0-9,]+/', '', $include );
+			$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+			$attachments = array();
+			foreach ( $_attachments as $key => $val ) {
+				$attachments[$val->ID] = $_attachments[$key];
+			}
+		} elseif ( !empty($exclude) ) {
+			$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+			$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		} else {
+			$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+		}
+
+		if ( empty($attachments) )
+			return '';
+
+		if ( is_feed() ) {
+			$output = "\n";
+			foreach ( $attachments as $att_id => $attachment )
+				$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+			return $output;
+		}
+
+		$itemtag = tag_escape($itemtag);
+		$captiontag = tag_escape($captiontag);
+		$columns = intval($columns);
+		$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+		$float = is_rtl() ? 'right' : 'left';
+
+		$selector = "gallery-{$instance}";
+
+		$gallery_style = $gallery_div = '';
+		if ( apply_filters( 'use_default_gallery_style', true ) )
+			$gallery_style = "
+			<style type='text/css'>
+				#{$selector} {
+					margin: auto;
+				}
+				#{$selector} .gallery-item {
+					float: {$float};
+					margin-top: 10px;
+					text-align: center;
+					width: {$itemwidth}%;
+				}
+				#{$selector} img {
+					border: 2px solid #cfcfcf;
+				}
+				#{$selector} .gallery-caption {
+					margin-left: 0;
+				}
+			</style>
+			<!-- see gallery_shortcode() in wp-includes/media.php -->";
+		$size_class = sanitize_html_class( $size );
+		$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+		$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+		var_dump( 'y' );
+		$i = 0;
+		foreach ( $attachments as $id => $attachment ) {
+			$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+			$output .= "<{$itemtag} class='gallery-item'>";
+			$output .= "
+				<{$icontag} class='gallery-icon'>
+					$link
+				</{$icontag}>";
+			if ( $captiontag && trim($attachment->post_excerpt) ) {
+				$output .= "
+					<{$captiontag} class='wp-caption-text gallery-caption'>
+					" . wptexturize($attachment->post_excerpt) . "
+					</{$captiontag}>";
+			}
+			$output .= "</{$itemtag}>";
+		}
+
+		$output .= "
+			</div>\n";
+			
+		return $output;
 	}
 	
 }	
