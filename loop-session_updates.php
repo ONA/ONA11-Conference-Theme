79a57e8e0f160dea1fcc4cdec76f66bdd797f137
Daniel Bachhuber <d@danielbachhuber.com>
2011-09-23 14:00:30 -0400

    Change up the presentation of the update text

--- loop-session_updates.php
+++ loop-session_updates.php
@@ -50,7 +50,7 @@
 		<?php endif; ?>
 		
 		<?php if ( !in_array( $post_format, array( 'gallery', 'standard' ) ) ): ?>
-		<div class="entry-meta"><span class="entry-author">By <?php ona11_author_posts_link(); ?></span> &mdash; <a href="<?php the_permalink(); ?>"><?php echo ucfirst( $post_format ); ?></a> &mdash; <span class="entry-timestamp"><?php ona11_timestamp( 'short', false ); ?></span></div>
+		<div class="entry-meta"><a href="<?php the_permalink(); ?>">&#8734; Permalink</a> &mdash; <span class="entry-timestamp"><?php ona11_timestamp( 'short', false ); ?></span> &mdash; <span class="entry-author">Posted by <?php ona11_author_posts_link(); ?></span></div>
 		<?php endif; ?>
 	
 	</div><!-- .post -->
